0db9105666222588f92a8bc31f5b3ed95e84fdec
qt <zekele.win.0@gmail.com>
2025-12-28 02:13:28 +0800

    Update html icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import "./layout.css";
-  import LogoIcon from "$lib/assets/logo.svg";
 
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={LogoIcon} />
+  <link rel="icon" href="/logo.png" sizes="any" type="image/png" />
+  <link rel="icon" href="/logo.svg" sizes="any" type="image/svg+xml" />
+
+  <link rel="apple-touch-icon" href="/logo.png" sizes="any" type="image/png" />
+
   <meta name="robots" content="index, follow" />
 </svelte:head>
 
